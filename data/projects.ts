f6755f13f20b535b7bbf48405ecaaad4a742e5ff
Bunny099 <jayeshkhuman121@gmail.com>
2026-01-07 23:36:28 +0530

    feat:data

---
+++ data/projects.ts
@@ -0,0 +1,36 @@
+
+
+
+export const projects = [
+  {
+    title: "LinkFly",
+    tech: "Next.js . PostgreSQL . Drizzle . BetterAuth . Vercel . Github . Axios . Docker",
+    image: "/linkfly.png",
+    link: "https://linkfly-three.vercel.app/",
+  },
+  {
+    title: "Officers",
+    tech: "Next.js . Typescript . PostgreSql . Prisma . NextAuth . TelegramBot . Axios . Vercel . Gitub . Docker ",
+    image: "/officers.png",
+    link: "https://officers-three.vercel.app/",
+  },
+  {
+    title: "Keshav-Digital",
+    tech: "Next.js . Typescript . PostgreSql . Pirsma . NextAuth . Tailwind . Docker . Axios . Vercel . Github",
+    image: "/keshavdigital.png",
+    link: "https://keshav-digital.vercel.app",
+  },
+  {
+    title: "TimeWise",
+    tech: "Next.js . Typescript . PostgreSql . Pirsma . NextAuth . Tailwind . Docker . Axios . Vercel . Github",
+    image: "/timewise.png",
+    link: "http://timewise-beta.vercel.app/",
+  },
+   {
+    title: "Portfolio-version1",
+    tech: "Next.js . Tailwind . Vercel . Github",
+    image: "/portfolio1.png",
+    link: "https://jayeshkhuman.in",
+  },
+];
+
