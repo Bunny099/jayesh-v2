0030a949c9816d943839fb8bc26cfcde7a3db96c
Bunny099 <jayeshkhuman121@gmail.com>
2026-01-08 16:58:33 +0530

    fix:icon visibility

--- components/layout/mode.tsx
+++ components/layout/mode.tsx
@@ -9,13 +9,14 @@ export function Mode() {
   const [mounted, setMounted] = useState(false);
   useEffect(() => { setMounted(true) }, []);
   if (!mounted) return null;
-  return ( <>
+  return (<>
 
-    <Button variant={"outline"} size="icon" className="relative rounded-full hover:cursor-pointer" onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}>
-      <Sun className="h-5 w-5 rotate-0 scale-100 transition-all  dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:scale-100 dark:rotate-0"/>  
+    <Button variant={"outline"} size="icon" className="rounded-full hover:cursor-pointer" onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}>
+
+      {resolvedTheme === "dark" ? (<Sun className="h-6 w-6 text-foreground" />) : (<Moon className="h-6 w-6 text-foreground" />
+      )}
     </Button>
-  
+
   </>
   )
 }
