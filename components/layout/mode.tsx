7da7cc0b20ea7ba6a9e229093e13c0490c76f458
Bunny099 <jayeshkhuman121@gmail.com>
2026-01-08 16:19:37 +0530

    UI(fix):remove drop-down

--- components/layout/mode.tsx
+++ components/layout/mode.tsx
@@ -1,40 +1,21 @@
 "use client"
-
-import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
-
+import { useState, useEffect } from "react"
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 
 export function Mode() {
-  const { setTheme } = useTheme()
+  const { resolvedTheme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => { setMounted(true) }, []);
+  if (!mounted) return null;
+  return ( <>
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant={"outline"} size="icon" className="rounded-full decoration-0 hover:cursor-pointer">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90 "  />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button variant={"outline"} size="icon" className="relative rounded-full hover:cursor-pointer" onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}>
+      <Sun className="h-5 w-5 rotate-0 scale-100 transition-all  dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:scale-100 dark:rotate-0"/>  
+    </Button>
+  
+  </>
   )
 }
