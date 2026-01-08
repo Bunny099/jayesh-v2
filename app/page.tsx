ee9c774e93d705973158d12261479a03c3309ea7
Bunny099 <jayeshkhuman121@gmail.com>
2026-01-08 16:24:59 +0530

    main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
-
+import { Navbar } from "@/components/layout/navbar";
+import { ProjectsScroller } from "@/components/layout/projectsScroller";
 
 export default function Home() {
   return (
-    <div>Jayesh</div>
+    <div className="bg-background min-h-screen w-full  ">
+      <Navbar />
+      <section className="mt-10">
+        <h2 className=" text-sm font-semibold tracking-widest text-neutral-400 px-8 sm:px-10 md:px-20 lg:px-50 xl:px-89">
+          THINGS I MADE
+        </h2>
+
+      </section>
+      <section>
+        <ProjectsScroller />
+      </section>
+
+    </div>
   );
 }
