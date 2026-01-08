ea7bcb1c0877408818f6e4185902fdf1ed402c1b
Bunny099 <jayeshkhuman121@gmail.com>
2026-01-08 16:21:10 +0530

    feat:projectsScroller component

---
+++ components/layout/projectsScroller.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useEffect, useRef } from "react";
+import { motion, useMotionValue } from "framer-motion";
+import { ProjectCard } from "./projectCard";
+import { projects } from "@/data/projects";
+
+export function ProjectsScroller() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0)
+  const speedRef = useRef(0.4)
+  function handleMoudeMove(e: React.MouseEvent) {
+    const bounds = containerRef.current!.getBoundingClientRect();
+    const center = bounds.x + bounds.width / 2;
+    const delta = e.clientX - center;
+    speedRef.current = delta * 0.002;
+  }
+
+  useEffect(() => {
+    let raf: number;
+    const loop = () => {
+      x.set(x.get() - speedRef.current)
+      raf = requestAnimationFrame(loop)
+    }
+    raf = requestAnimationFrame(loop);
+    return () => cancelAnimationFrame(raf)
+
+  }, [x])
+  return <div
+    ref={containerRef}
+    onMouseMove={handleMoudeMove}
+    className="relative overflow-hidden py-12">
+    <motion.div
+      style={{ x }}
+      className="flex gap-8 w-max"
+      onMouseEnter={() => { }}
+      onMouseLeave={() => { }}
+    >
+      {[...projects, ...projects].map((p, i) => (
+        <ProjectCard key={i} project={p} />
+      ))}
+    </motion.div>
+    
+  </div>
+}
