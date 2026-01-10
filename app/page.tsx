1e1328238db5886c66f3eee984451cba46a0a61f
Bunny099 <jayeshkhuman121@gmail.com>
2026-01-10 22:37:55 +0530

    update:updated page.tsc

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/layout/navbar";
 import { ProjectsScroller } from "@/components/layout/projectsScroller";
 import TechBadge from "@/components/layout/techBadges";
+import Work from "@/components/layout/work";
 import { Mail } from "lucide-react";
 import Link from "next/link";
 export default function Home() {
@@ -11,41 +12,45 @@ export default function Home() {
         <h2 className=" text-sm font-semibold tracking-widest text-neutral-400 px-8 sm:px-10 md:px-20 lg:px-50 xl:px-89">
           THINGS I MADE
         </h2>
-
       </section>
       <section>
         <ProjectsScroller />
       </section>
 
-    <section className="mt-10">
-      <h2 className=" text-sm font-semibold tracking-widest text-neutral-400 px-8 sm:px-10 md:px-20 lg:px-50 xl:px-89">
-          WORK | CONTRIBUTION
-        </h2>
-      
-    </section>
-    <section className="mt-10">
-      <h2 className=" text-sm font-semibold tracking-widest text-neutral-400 px-8 sm:px-10 md:px-20 lg:px-50 xl:px-89">
-          TECH
-        </h2>
-        <TechBadge/>
-      
-    </section>
-     <section className="mt-10">
-      <h2 className=" text-sm font-semibold tracking-widest text-neutral-400 px-8 sm:px-10 md:px-20 lg:px-50 xl:px-89">
-          ABOUT
-        </h2>
-      
-    </section>
-     <section className="py-10 ">
-      <h2 className=" text-sm font-semibold tracking-widest text-neutral-400 px-8 sm:px-10 md:px-20 lg:px-50 xl:px-89">
-          CONTACT
-        </h2>
-        <div className="px-8 sm:px-10 md:px-20 lg:px-50 xl:px-89 mt-4 ">
-          <Link href={"mailto:jayeshkhuman121@gmail.com"} target="_blank"><Mail  className="border text-muted-foreground rounded-full h-9 w-9 p-2"/></Link>
-          
+      <section className="mt-10">
+        <div className=" px-8 sm:px-10 md:px-20 lg:px-50 xl:px-89">
+          <h2 className=" text-sm font-semibold tracking-widest text-neutral-400">
+            WORK & CONTRIBUTION
+          </h2>
+          <Work />
         </div>
-      
-    </section>
+      </section>
+      <section className="mt-10">
+        <div className=" px-8 sm:px-10 md:px-20 lg:px-50 xl:px-89">
+          <h2 className=" text-sm font-semibold tracking-widest text-neutral-400">
+            TECH
+          </h2>
+          <TechBadge />
+        </div>
+      </section>
+      <section className="mt-10">
+        <div className="px-8 sm:px-10 md:px-20 lg:px-50 xl:px-89">
+          <h2 className=" text-sm font-semibold tracking-widest text-neutral-400 ">
+            ABOUT
+          </h2>
+        </div>
+      </section>
+
+      <section className="py-10 mt-10">
+        <div className="px-8 sm:px-10 md:px-20 lg:px-50 xl:px-89">
+          <h2 className=" text-sm font-semibold tracking-widest text-neutral-400 ">
+            CONTACT
+          </h2>
+          <div className="mt-4 ">
+            <Link href={"mailto:jayeshkhuman121@gmail.com"} target="_blank"><Mail className="border text-muted-foreground rounded-full h-9 w-9 p-2" /></Link>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
