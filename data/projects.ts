986cb64b5010266bd0c3fb55ec87f7295cc1be0e
Bunny099 <jayeshkhuman121@gmail.com>
2026-01-11 21:58:49 +0530

    github links added

--- data/projects.ts
+++ data/projects.ts
@@ -7,30 +7,35 @@ export const projects = [
     tech: "Next.js . PostgreSQL . Drizzle . BetterAuth . Vercel . Github . Axios . Docker",
     image: "/linkfly.png",
     link: "https://linkfly-three.vercel.app/",
+    github:"https://github.com/Bunny099/linkfly"
   },
   {
     title: "Officers",
     tech: "Next.js . Typescript . PostgreSql . Prisma . NextAuth . TelegramBot . Axios . Vercel . Gitub . Docker ",
     image: "/officers.png",
     link: "https://officers-three.vercel.app/",
+    
   },
   {
     title: "Keshav-Digital",
     tech: "Next.js . Typescript . PostgreSql . Pirsma . NextAuth . Tailwind . Docker . Axios . Vercel . Github",
     image: "/keshavdigital.png",
     link: "https://keshav-digital.vercel.app",
+    github:"https://github.com/Bunny099/keshav-digital"
   },
   {
     title: "TimeWise",
     tech: "Next.js . Typescript . PostgreSql . Pirsma . NextAuth . Tailwind . Docker . Axios . Vercel . Github",
     image: "/timewise.png",
     link: "http://timewise-beta.vercel.app/",
+    github:"https://github.com/Bunny099/timewise"
   },
    {
     title: "Portfolio-version1",
     tech: "Next.js . Tailwind . Vercel . Github",
     image: "/portfolio1.png",
     link: "https://jayeshkhuman.in",
+    github:"https://github.com/Bunny099/jayesh"
   },
 ];
 
