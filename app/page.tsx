cbe7bc03553dbd9b033c18a8b68e02b80ea2bdf1
Bunny099 <jayeshkhuman121@gmail.com>
2026-01-11 19:36:33 +0530

    feat:about section added

--- app/page.tsx
+++ app/page.tsx
@@ -35,13 +35,14 @@ export default function Home() {
       </section>
       <section className="mt-10">
         <div className="px-8 sm:px-10 md:px-20 lg:px-50 xl:px-89">
-          <h2 className=" text-sm font-semibold tracking-widest text-neutral-400 ">
+          <h2 className="py-2 text-sm font-semibold tracking-widest text-neutral-400 ">
             ABOUT
           </h2>
+          <p className=" text-sm text-accent-foreground dark:text-neutral-400">I'm drawn to the part of building where things break, don't make sense, and force you to slow down and think. I mostly enjoy working on backend problems, where half-baked logic simply doesn't work and understanding what you're writing actually matters. I don't care much for fake signals or surface-level polish  if I can't understand my own code after some time, I consider it a failure.</p>
         </div>
       </section>
 
-      <section className="py-10 mt-10">
+      <section className="py-10 mt-2">
         <div className="px-8 sm:px-10 md:px-20 lg:px-50 xl:px-89">
           <h2 className=" text-sm font-semibold tracking-widest text-neutral-400 ">
             CONTACT
