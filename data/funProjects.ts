d864004d797cd673855bfc189aa5e02fc3b71f97
Bunny099 <jayeshkhuman121@gmail.com>
2026-06-19 00:04:24 +0530

    feat: new project added

--- data/funProjects.ts
+++ data/funProjects.ts
@@ -1,10 +1,17 @@
 
 
-export const funProjects =[{ 
+export const funProjects =[{
+    title:"Voluma",
+    tech:"Next.js, fastify, Typescript, Solana, Websocket",
+    link:"https://voluma.online",
+    github:"https://github.com/Bunny099/voluma",
+    description:"Voluma is a real-time on-chain automation for Solana."
+    },
+    { 
     title: "Dishcover",
     tech: "Next.js, Node.js, TypeScript, Express, Zod, Lingo.dev",
     link: "https://dishcover-tk3e.vercel.app/",
     github: "https://github.com/Bunny099/dishcover",
     description:
       "AI-powered multilingual food discovery app with structured dish intelligence and localization workflows.",
-  },]
+  }]
